d3b8b61ea8628c5a9863c3db90c064b919a36cc1
Ian Saucy <iansaucy@gmail.com>
2020-09-06 09:54:07 -0400

    Fix startup script to specify .env file for docker correctly

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -4,12 +4,12 @@
 if [ "$DEPLOYMENT_GROUP_NAME" == "SCDAO-API-PROD" ]
 then
   # Just start app, don't want to automatically upgrade DB in production
-  docker-compose -f /srv/scdao-api/docker-compose.yml -f /srv/scdao-api/docker-compose.prod.yml up --env-file=/srv/scdao-api/.env -d > /dev/null 2> /dev/null < /dev/null &
+  docker-compose -f /srv/scdao-api/docker-compose.yml -f /srv/scdao-api/docker-compose.prod.yml --env-file /srv/scdao-api/.env up -d > /dev/null 2> /dev/null < /dev/null &
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "SCDAO-API-DEV" ]
 then
   # Run DB upgrade and start app
   docker-compose run --rm backend alembic upgrade head
-  docker-compose -f /srv/scdao-api/docker-compose.yml -f /srv/scdao-api/docker-compose.dev.yml up --env-file=/srv/scdao-api/.env -d > /dev/null 2> /dev/null < /dev/null &
+  docker-compose -f /srv/scdao-api/docker-compose.yml -f /srv/scdao-api/docker-compose.dev.yml --env-file /srv/scdao-api/.env up -d > /dev/null 2> /dev/null < /dev/null &
 fi
