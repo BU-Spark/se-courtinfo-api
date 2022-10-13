9cdc56ac86afa64926b0b9e79c6b768c2ffb8e8a
toucham <thachathumam@gmail.com>
2022-10-13 16:08:07 -0400

    removed redis & celery
    commented out logic related to celery & redis

--- backend/app/api/api_v1/routers/uploads.py
+++ backend/app/api/api_v1/routers/uploads.py
@@ -5,32 +5,34 @@ from app.api.api_v1.uploads.uploads_utils import verify_uploaded_file_type
 from app.core import config
 from app.core.auth import get_current_user
 from app.schemas.user_schemas import User
-from app.tasks import handle_criminal_complaint_task
 from backend.app.document_ai import process_ccf_document
 
 uploads_router = u = APIRouter()
 
-@u.post(
-    "/ccf",
-    dependencies=[Depends(verify_uploaded_file_type)],
-    responses={400: {"description": "Returned when the uploaded file does not match the valid params"}},
-)
-def upload_criminal_complaint(
-        file: UploadFile = File(...),
-        current_user: User = Depends(get_current_user),
-):
-    """
-    Upload a Criminal Complaint Form via HTTP form
-    \n
-    Upload size is limited to 25MB and type must be, pdf, jpg, or png
 
-    :return Returns a task_id which can be used to track the status of the submission, check for errors etc.
-    see the /tasks route for details on how to lookup the status using the returned ID.
-    """
-    path = handle_upload_file(file)
-    string_path = str(path)
-    res = handle_criminal_complaint_task.apply_async(args=(string_path, current_user.id, "test", config.S3_BUCKET_NAME))
-    return {"job id": res.task_id}
+# Currently commented out since it uses the old OCR pipeline
+
+# @u.post(
+#     "/ccf",
+#     dependencies=[Depends(verify_uploaded_file_type)],
+#     responses={400: {"description": "Returned when the uploaded file does not match the valid params"}},
+# )
+# def upload_criminal_complaint(
+#         file: UploadFile = File(...),
+#         current_user: User = Depends(get_current_user),
+# ):
+#     """
+#     Upload a Criminal Complaint Form via HTTP form
+#     \n
+#     Upload size is limited to 25MB and type must be, pdf, jpg, or png
+
+#     :return Returns a task_id which can be used to track the status of the submission, check for errors etc.
+#     see the /tasks route for details on how to lookup the status using the returned ID.
+#     """
+#     path = handle_upload_file(file)
+#     string_path = str(path)
+#     res = handle_criminal_complaint_task.apply_async(args=(string_path, current_user.id, "test", config.S3_BUCKET_NAME))
+#     return {"job id": res.task_id}
 
 @u.post(
     "/ddi",
