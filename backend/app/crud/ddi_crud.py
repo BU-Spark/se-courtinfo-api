73698c4c9173a6f2eeaa5a10dc81ad97a709cd8d
aydongcheng <980588536@qq.com>
2022-12-09 15:39:22 -0500

    modify ddi crud

--- backend/app/crud/ddi_crud.py
+++ backend/app/crud/ddi_crud.py
@@ -20,10 +20,14 @@ def get_ddi(db: Session, ddi_id: UUID4) -> form_models.DefendantDemoInfo:
         raise HTTPException(status_code=404, detail="ddi not found")
     return ddi
 
-def update_ddi(db: Session, updater: str, ddi: ddi_schemas.DefendantDemoInfoCreate) -> form_models.DefendantDemoInfo:
+def get_all_ddi(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(form_models.DefendantDemoInfo).order_by(form_models.DefendantDemoInfo.created_at.asc()).offset(
+        skip).limit(limit).all()
+
+def update_ddi(db: Session, updater: str, ddi: ddi_schemas.DefendantDemoInfoUpdate) -> form_models.DefendantDemoInfo:
     db_ddi = get_ddi(db, ddi.id)
     if not db_ddi:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Criminal Complaint form not found")
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="ddi form not found")
     update_data = ddi.dict(exclude_unset=True)
     for key, value in update_data.items():
         setattr(db_ddi, key, value)
