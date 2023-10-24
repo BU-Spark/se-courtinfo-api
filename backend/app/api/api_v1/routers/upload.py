aa9ed5867d97a81bac689da403a9ab8179c1ca06
MollyZhou <mollyzhou001@gmail.com>
2023-10-24 17:13:50 -0400

    upload api updated

--- backend/app/api/api_v1/routers/upload.py
+++ backend/app/api/api_v1/routers/upload.py
@@ -2,27 +2,27 @@ from typing import Optional
 from fastapi import APIRouter, Depends, File, UploadFile
 from pydantic.types import UUID4
 
-from app.api.api_v1.uploads.uploads_utils import handle_upload_file
+from app.api.api_v1.uploads.uploads_utils import handle_upload_files
 from app.api.api_v1.uploads.uploads_utils import verify_uploaded_file_type
-from app.document_ai.process import process_ddi_document
 from app.core.auth import get_current_active_user, get_current_active_superuser
+# from app.document_ai.process import process_ddi_document
 
 uploads_router = u = APIRouter()
 
 @u.post(
     "/upload",
-    dependencies=[Depends(verify_uploaded_file_type)],
+    dependencies = [Depends(verify_uploaded_file_type)],
     responses = {400: {"description": "Returned when the uploaded file does not match the valid params"}}
 )
 def upload_form(
     upload_id: Optional[UUID4],
     form_type: int,
-    file: UploadFile = File(...),
-    current_user=Depends(get_current_active_superuser),
+    files: list[UploadFile],
+    current_user = Depends(get_current_active_superuser),
 ):
-    path = handle_upload_file(file)
+    path = handle_upload_files(files)
     string_path = str(path)
-    return {"status": "File uploaded successfully", "url": string_path}
+    return {"status": "succeeded", "url": string_path}
 
 # response model for return
 # use async def (: Item) -> Item
