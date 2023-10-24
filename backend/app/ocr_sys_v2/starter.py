2c40ae1762214584753fa4feb500ebd7241efef8
Jason Wang <jasonwang12@icloud.com>
2023-10-24 11:43:57 -0400

    try

---
+++ backend/app/ocr_sys_v2/starter.py
@@ -0,0 +1,59 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+'''
+
+This file will throw a file into OCR and read it.
+Returns an string, which is the block of text from the file.
+
+'''
+'''
+Authenticate
+Authenticates your credentials and creates a client.
+'''
+subscription_key = os.environ["VISION_KEY"]
+endpoint = os.environ["VISION_ENDPOINT"]
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+'''
+END - Authenticate
+'''
+
+'''
+OCR: Read File using the Read API, extract text - remote
+This example will extract text in an image, then print results, line by line.
+This API call can also extract handwriting style text (not shown).
+'''
+print("===== Read File - remote =====")
+# Get an image with text
+read_image_url = "https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png"
+
+# Call API with URL and raw response (allows you to get the operation location)
+read_response = computervision_client.read(read_image_url,  raw=True)
+
+# Get the operation location (URL with an ID at the end) from the response
+read_operation_location = read_response.headers["Operation-Location"]
+# Grab the ID from the URL
+operation_id = read_operation_location.split("/")[-1]
+
+# Call the "GET" API and wait for it to retrieve the results 
+while True:
+    read_result = computervision_client.get_read_result(operation_id)
+    if read_result.status not in ['notStarted', 'running']:
+        break
+    time.sleep(1)
+
+# Print the detected text, line by line
+if read_result.status == OperationStatusCodes.succeeded:
+    for text_result in read_result.analyze_result.read_results:
+        for line in text_result.lines:
+            print(line.text)
+            print(line.bounding_box)
+print()
--- backend/app/ocr_sys_v2/temp.py
+++ backend/app/ocr_sys_v2/temp.py
@@ -1,2 +1,5 @@
 #placeholder
+import re
+from app.schemas.cc_schemas import CriminalComplaintBase
 
+#def 
