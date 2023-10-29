f98f90d5e2081bf4b6527e05d8d0e5b4d5a9d11e
Jason Wang <jasonwang12@icloud.com>
2023-10-29 13:55:28 -0400

    cc schemify

--- backend/app/ocr_sys_v2/cc_schemify.py
+++ backend/app/ocr_sys_v2/cc_schemify.py
@@ -40,6 +40,106 @@ def get_defen_name(text: str) -> Optional[str]:
     else:
         return None
 
+def get_defen_adr(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the defendant's address.
+    '''
+    defen_adr = re.search(r'Defendant Address:\s*(.+)', text)
+    if defen_adr:
+        return defen_adr.group(1)
+    else:
+        return None
+
+def get_defen_DOB(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the defendant's date of birth.
+    '''
+    defen_DOB = re.search(r'Defendant DOB:\s*(.+)', text)
+    if defen_DOB:
+        return defen_DOB.group(1)
+    else:
+        return None
+    
+def get_court_name_adr(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the court name and address.
+    '''
+    court_name_adr = re.search(r'Court Name and Address:\s*(.+)', text)
+    if court_name_adr:
+        return court_name_adr.group(1)
+    else:
+        return None
+
+def get_complaint_issued_date(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the complaint issued date.
+    '''
+    complaint_issued_date = re.search(r'Complaint Issued Date:\s*(.+)', text)
+    if complaint_issued_date:
+        return complaint_issued_date.group(1)
+    else:
+        return None
+
+def get_offense_date(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the offense date.
+    '''
+    offense_date = re.search(r'Offense Date:\s*(.+)', text)
+    if offense_date:
+        return offense_date.group(1)
+    else:
+        return None
+
+def get_arrest_date(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the arrest date.
+    '''
+    arrest_date = re.search(r'Arrest Date:\s*(.+)', text)
+    if arrest_date:
+        return arrest_date.group(1)
+    else:
+        return None
+
+def get_next_event_date(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the next event date.
+    '''
+    next_event_date = re.search(r'Next Event Date:\s*(.+)', text)
+    if next_event_date:
+        return next_event_date.group(1)
+    else:
+        return None
+
+def get_police_incident_num(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the police incident number.
+    '''
+    police_incident_num = re.search(r'Police Incident #:\s*(.+)', text)
+    if police_incident_num:
+        return police_incident_num.group(1)
+    else:
+        return None
+
+def get_OBTN(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the OBTN.
+    '''
+    OBTN = re.search(r'OBTN:\s*(.+)', text)
+    if OBTN:
+        return OBTN.group(1)
+    else:
+        return None
+
+def get_offense_codes(text: str) -> Optional[str]:
+    '''
+    This function takes in the text from the image and returns the offense codes.
+    '''
+    offense_codes = re.search(r'Offense Codes:\s*(.+)', text)
+    if offense_codes:
+        return offense_codes.group(1)
+    else:
+        return None
+
 # MIGHT PUT THESE INTO 2 SEPARATE FILES  
 def cc_schema_fill(text: str) -> CriminalComplaintBase:
     '''
@@ -69,20 +169,21 @@ def cc_schema_fill(text: str) -> CriminalComplaintBase:
     defen_name = get_defen_name(text)
     defen_adr = get_defen_adr(text)
     defen_DOB = get_defen_DOB(text)
-    court_name_adr = get_court_name_adr(text)
     complaint_issued_date = get_complaint_issued_date(text)
     offense_date = get_offense_date(text)
     arrest_date = get_arrest_date(text)
-    offense_city = get_offense_city(text)
-    offense_adr = get_offense_adr(text)
-    police_dept = get_police_dept(text)
+    next_event_date = get_next_event_date(text)
+    court_name_adr = get_court_name_adr(text)
     police_incident_num = get_police_incident_num(text)
     OBTN = get_OBTN(text)
-    PCF_number = get_PCF_number(text)
-    defen_xref_id = get_defen_xref_id(text)
     offense_codes = get_offense_codes(text)
-    return
-    #return CriminalComplaintBase(docket=docket, ...)
+    raw_text = text
+    return CriminalComplaintBase(docket=docket, defen_name=defen_name, defen_adr=defen_adr, 
+                                 defen_DOB=defen_DOB, complaint_issued_date=complaint_issued_date, 
+                                 offense_date=offense_date, arrest_date=arrest_date, 
+                                 next_event_date=next_event_date, court_name_adr=court_name_adr, 
+                                 police_incident_num=police_incident_num, OBTN=OBTN, offense_codes=offense_codes,
+                                 raw_text=raw_text)
 
 #def fit_schema(text: str):
 #    if text
