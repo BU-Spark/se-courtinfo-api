ed6739373d729faf0cc01b0cd478660c8b40be2d
AdiBhan <abhan03@bu.edu>
2023-11-28 17:45:24 -0500

    Updated DDI schema and model

--- backend/app/models/form_models.py
+++ backend/app/models/form_models.py
@@ -5,7 +5,6 @@ from app.db.base_class import Base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
 
-
 class CriminalComplaint(Base):
     """
     This represents how CC are stored in the database, each field is a column with certain properties etc.
@@ -42,6 +41,7 @@ class CriminalComplaint(Base):
     img_key = Column(String)
     aws_bucket = Column(String)
 
+
 class DefendantDemoInfo(Base):
     """
     This represents how DDI are stored in the database, each field is a column with certain properties etc.
@@ -50,11 +50,15 @@ class DefendantDemoInfo(Base):
     __tablename__ = "defendant_demographic_info"
 
     ddi_id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    date_of_birth = Column(String)
+
     created_by = Column(UUID(as_uuid=True), ForeignKey('users.id'))
     updated_by = Column(UUID(as_uuid=True), ForeignKey('users.id'))
     created_at = Column(DateTime(timezone=True), server_default=utcnow())
     updated_at = Column(DateTime(timezone=True), onupdate=utcnow())
-    zip: Column(Integer)
+    zip_code: Column(Integer)
     race: Column(String)
     sex: Column(String)
     recommendation: Column(String)
