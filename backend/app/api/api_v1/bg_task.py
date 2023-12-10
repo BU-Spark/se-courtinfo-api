62c0187c9723eb95ccb1bc86d33bc59ef9a09ae5
Jason Wang <jasonwang12@icloud.com>
2023-12-09 19:15:44 -0500

    logging bg task

--- backend/app/api/api_v1/bg_task.py
+++ backend/app/api/api_v1/bg_task.py
@@ -1,15 +1,28 @@
 from fastapi import BackgroundTasks, FastAPI
 from typing import List
 from pathlib import Path
+import logging
+from app.ocr_sys_v2.ocr_read import read_text
 
 
 app = FastAPI()
 
-from app.ocr_sys_v2.ocr_read import read_text
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def process_images_background(image_paths: List[Path], background_tasks: BackgroundTasks):
     # Queue the top-level function for each image as a background task
     for image_path in image_paths:
-        background_tasks.add_task(read_text, image_path)
+        background_tasks.add_task(process_image, image_path)
 
+def process_image(image_path: Path):
+    try:
+        # Call the read_text function
+        result = read_text(image_path)
+
+        # Log the result
+        logger.info(f"Image processed: {image_path}, Result: {result}")
+    except Exception as e:
+        # Log any exceptions that occur during processing
+        logger.error(f"Error processing image {image_path}: {e}")
